79a8d8fcd6e8858c1b609c20b5331fd0d0f8e486
kukjin <thater114@gmail.com>
2024-08-26 22:02:26 +0900

    [pythontools][ch2-5] data type and structure v1

---
+++ 파이썬의도구들/0_파이썬의기본/4_datatype_datastruct.py
@@ -0,0 +1,39 @@
+
+print(10)
+a = 20
+print(a)
+
+print(type(a))
+
+
+# 정수
+var_int = 10
+print(var_int)
+print(type(var_int))
+
+#실수
+var_float = 10.5
+print(var_float)
+print(type(var_float))
+
+#문자열
+var_string = "문자열입니다."
+print(var_string)
+print(type(var_string))
+
+#불리언
+var_bool = True
+print(var_bool)
+print(type(var_bool))
+
+#리스트
+var_list = [1,2,'a',['a',1,2]]
+print(var_list)
+print(type(var_list))
+
+#튜플
+var_tuple = (37.5665, 126.9780)
+print(var_tuple)
+print(type(var_tuple))
+# var_tuple[0] = 1
+
