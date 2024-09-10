aa3f29b5f918d0ec07e8bf52f3e96b3c01e3616e
kukjin <thater114@gmail.com>
2024-09-10 23:04:19 +0900

    [pythontools][ch2-5] data type and structure v1

--- 파이썬의도구들/0_파이썬의기본/4_datatype_datastruct.py
+++ 파이썬의도구들/0_파이썬의기본/4_datatype_datastruct.py
@@ -27,9 +27,13 @@ print(var_bool)
 print(type(var_bool))
 
 #리스트
-var_list = [1,2,'a',['a',1,2]]
+var_list = [1,2,3]
 print(var_list)
 print(type(var_list))
+var_list.
+var_list2 = [1,2,3,'a',[1,2,'b']]
+print(var_list2)
+print(type(var_list2))
 
 #튜플
 var_tuple = (37.5665, 126.9780)
@@ -37,3 +41,11 @@ print(var_tuple)
 print(type(var_tuple))
 # var_tuple[0] = 1
 
+#딕셔너리
+var_dict={'철수':90,'영희':85,'민수':95}
+print(var_dict)
+print(type(var_dict))
+
+#세트
+var_set = {1, 2, 2, 3, 3, 4, 4}
+print(var_set)
