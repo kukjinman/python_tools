993fc097d34d02addfa1070aa66f000e306b70ee
kukjinm <thater114@gmail.com>
2025-01-06 20:52:21 +0900

    pandas finish and 7 opencv commit

--- 파이썬의도구들/CH3_파이썬의도구들_라이브러리/6_pandas/6_pandas.py
+++ 파이썬의도구들/CH3_파이썬의도구들_라이브러리/6_pandas/6_pandas.py
@@ -1,15 +1,19 @@
+#1 pandas 라이브러리를 pd로 가져옵니다.
 import pandas as pd
 
-# 엑셀 파일 경로
-file_path = '학생리스트.xlsx'
+#2 엑셀 파일 열기
+df = pd.read_excel('학생리스트.xlsx', header=1)
 
-# 엑셀 파일 읽기
-# df = pd.read_excel(file_path, header=1)
-df = pd.read (file_path, con='sqlite:///example.db')
+#3 df 결과 출력
+print("df 결과")
+print(type(df))
 print(df)
-# 각 행을 출력
+
+print("-------------------------------")
+#4 df.iterrows()의 각 행의 데이터 값을 출력
+print("df.iterrows() 결과")
 for index, row in df.iterrows():
     print(f"{index}번째")
     print(f"학생이름: {row['학생이름']}, 학년: {row['학년']}, 반: {row['반']}")
 
-
+pd.read_j
