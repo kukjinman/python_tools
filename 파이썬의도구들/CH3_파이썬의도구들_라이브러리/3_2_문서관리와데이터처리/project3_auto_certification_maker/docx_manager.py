310800000192ef6701ec54e91f7105a6f5b23939
kukjinm <thater114@gmail.com>
2025-02-06 21:38:41 +0900

    project3 code commit v5 project4 resource files

--- 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_2_문서관리와데이터처리/project3_auto_certification_maker/docx_manager.py
+++ 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_2_문서관리와데이터처리/project3_auto_certification_maker/docx_manager.py
@@ -6,13 +6,12 @@ output_directory = 'output'
 #4 create_certificates 함수
 def create_certificates(list_var, template_path):
 
-    #5 list_var 순회
     for item in list_var:
         number_, class_, name_ = item
 
+        #5 수료증의 number, class, name 값 교체
         doc = Document(template_path)
 
-        #6 수료증의 number, class, name 값 교체
         for paragraph in doc.paragraphs:
             for run in paragraph.runs:
                 if 'number' in run.text:
@@ -25,6 +24,6 @@ def create_certificates(list_var, template_path):
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
 
-        #7 수정된 문서를 새 파일로 저장
+        #6 수정된 문서를 새 파일로 저장
         output_path = output_directory + f'/certificate_{name_}.docx'
         doc.save(output_path)
