3d85f239e2cbcf24023db7adcc902bb4cc8763a9
kukjinm <thater114@gmail.com>
2025-02-09 14:08:19 +0900

    toolG smtplib commit v1

--- 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_3_E-mail작업/toolG_smtplib/4_smtplib.py
+++ 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_3_E-mail작업/toolG_smtplib/4_smtplib.py
@@ -1,46 +1,46 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
+from email.header import Header
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-# Email details
-from_address = "kukjinman@gmail.com"
-to_address = "kukjinman2@gmail.com"
-subject = "Test Email"
-body = "This is a test email with an attachment."
-
-# Create the email
-msg = MIMEMultipart()
-msg['From'] = from_address
-msg['To'] = to_address
-msg['Subject'] = subject
-msg.attach(MIMEText(body, 'plain'))
-
-# Attach the file
-filename = "test.txt"
-with open(filename, "rb") as attachment:
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', f"attachment; filename= {filename}")
-    msg.attach(part)
-
-# SMTP server configuration
+# SMTP 서버 설정
 smtp_server = "smtp.gmail.com"
-smtp_port = 587
 smtp_user = "kukjinman@gmail.com"
 smtp_password = "mbns gavv xekv hcpu"  # Replace with your actual password
 
-# Send the email
+
+# Email 설정
+from_address = smtp_user
+to_address = "kukjinman2@gmail.com"
+subject = "Test Email"
+body = "파이썬 도구G 첨부파일 예시 메일입니다."
+
+# Email 객체 생성
+msg = MIMEMultipart()
+msg['From'] = from_address
+msg['To'] = to_address
+msg['Subject'] = Header(s='파일첨부 메일송신 테스트', charset='utf-8')
+msg.attach(MIMEText(body, 'plain',_charset='utf-8'))
+
+# 첨부파일 추가
+filename = "test.txt"
+part = MIMEBase('application', 'octet-stream')
+attachment = open(filename, "rb")
+part.set_payload(attachment.read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', f"attachment; filename= {filename}")
+msg.attach(part)
+
+
+# SMTP 서버 연결 및 이메일 전송
 try:
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()
+    server = smtplib.SMTP_SSL(smtp_server)
     server.login(smtp_user, smtp_password)
-    text = msg.as_string()
-    server.sendmail(from_address, to_address, text)
+    server.send_message(msg)
+
     print("Email sent successfully")
 except Exception as e:
     print(f"Failed to send email: {e}")
-finally:
-    server.quit()
+server.quit()
