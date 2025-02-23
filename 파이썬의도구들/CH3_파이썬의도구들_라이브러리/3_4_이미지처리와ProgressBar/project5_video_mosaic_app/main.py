b54f109567c60c1bbe7775ce861730270b1d0bd8
kukjinm <thater114@gmail.com>
2025-02-23 21:45:58 +0900

    project 5 commit final

--- 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_4_이미지처리와ProgressBar/project5_video_mosaic_app/main.py
+++ 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_4_이미지처리와ProgressBar/project5_video_mosaic_app/main.py
@@ -1,24 +1,10 @@
-# non 클래스 버전
-from opencv_manager import initialize_video_paths, make_mosaic_video, close_video
+from opencv_manager import VideoMosaic
 
-face_cascade, cap, out, total_frame = initialize_video_paths('sample_video.mp4', 'output_video.avi')
-make_mosaic_video(face_cascade, cap, out, total_frame)
-close_video(cap, out)
+video_path = 'sample_video.mp4'
+output_path = 'output_video.avi'
 
+#0 VideoMosaic 클래스 객체 생성
+video_mosaic = VideoMosaic(video_path, output_path)
 
-
-# # 클래스 버전
-# from opencv_manager_classversion import VideoMosaic
-#
-# video_path = 'sample_video.mp4'
-# output_path = 'output_video.avi'
-#
-# video_mosaic = VideoMosaic(video_path, output_path)
-# video_mosaic.make_mosaic_video()
-
-
-
-
-
-
-
+#2 make_mosaic_video 함수 호출
+video_mosaic.make_mosaic_video()
--- 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_4_이미지처리와ProgressBar/project5_video_mosaic_app/opencv_manager.py
+++ 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_4_이미지처리와ProgressBar/project5_video_mosaic_app/opencv_manager.py
@@ -1,53 +1,54 @@
 import cv2
 from tqdm import tqdm
 
-def initialize_video_paths(input_path, output_path):
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    cap = cv2.VideoCapture(input_path)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(output_path, fourcc, cap.get(cv2.CAP_PROP_FPS), (int(cap.get(3)), int(cap.get(4))))
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+class VideoMosaic:
 
-    return face_cascade, cap, out, total_frames
+    #1 초기화 함수
+    def __init__(self, video_path, output_path):
+        self.video_path = video_path
+        self.output_path = output_path
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        self.cap = cv2.VideoCapture(video_path)
+        self.fourcc = cv2.VideoWriter_fourcc('XVID')
+        self.out = cv2.VideoWriter(output_path, self.fourcc, self.cap.get(cv2.CAP_PROP_FPS),
+                                   (int(self.cap.get(3)), int(self.cap.get(4))))
+        self.total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
+    #6 apply_mosaic 함수
+    def apply_mosaic(self, image, x, y, w, h):
+        roi = image[y:y+h, x:x+w]
+        roi = cv2.resize(roi, (int(w//20), int(h//20)), interpolation=cv2.INTER_LINEAR)
+        roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_NEAREST)
+        image[y:y+h, x:x+w] = roi
+        return image
 
-# 모자이크 효과를 적용하는 함수
-def apply_mosaic(image, x, y, w, h):
-    roi = image[y:y+h, x:x+w]
-    roi = cv2.resize(roi, (int(w//20), int(h//20)), interpolation=cv2.INTER_LINEAR)
-    roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_NEAREST)
-    image[y:y+h, x:x+w] = roi
+    #7 close_video 함수
+    def close_video(self):
+        self.cap.release()
+        self.out.release()
+        cv2.destroyAllWindows()
 
-    return image
+    #3 make_mosaic_video 함수
+    def make_mosaic_video(self):
 
-def close_video(cap, out):
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
+        #4 tqdm을 사용하여 비디오 처리 진행상황 표시
+        with tqdm(total=self.total_frames, desc="Processing Video") as pbar:
+            while self.cap.isOpened():
+                ret, frame = self.cap.read()
+                if not ret:
+                    break
 
-def make_mosaic_video(face_cascade, cap, out, total_frames):
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(20, 20))
 
-    # 진행률 표시와 함께 비디오 모자이크 처리
-    with tqdm(total=total_frames, desc="Processing Video") as pbar:
+                #5 얼굴에 모자이크 처리
+                for (x, y, w, h) in faces:
 
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
+                    #6
+                    frame = self.apply_mosaic(frame, x, y, w, h)
 
-            # 프레임을 그레이스케일로 변환
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                self.out.write(frame)
+                pbar.update(1)
 
-            # 프레임에서 얼굴 검출
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(20, 20))
-
-            # 검출된 얼굴에 모자이크 적용
-            for (x, y, w, h) in faces:
-                frame = apply_mosaic(frame, x, y, w, h)
-
-            # 출력 비디오에 모자이크 된 프레임 쓰기
-            out.write(frame)
-
-            # 진행 된 frame 수 업데이트
-            pbar.update(1)
-
+        #7
+        self.close_video()
