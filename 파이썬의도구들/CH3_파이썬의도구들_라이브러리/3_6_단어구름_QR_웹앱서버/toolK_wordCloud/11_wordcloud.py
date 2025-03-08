f1f388ca9de09b99ee8a739cfa492624c8beff89
kukjinm <thater114@gmail.com>
2025-03-09 00:29:10 +0900

    toolK wordcloud commit v3

--- 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_6_단어구름_QR_웹앱서버/toolK_wordCloud/11_wordcloud.py
+++ 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_6_단어구름_QR_웹앱서버/toolK_wordCloud/11_wordcloud.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-#1 단어 모음이 있는 words_data.py에서 words import
+#1 words_data.py 파일의 words 불러오기
 from words_data import words
 
 #2 words의 단어마다 ' ' 띄어쓰기 부여
@@ -12,10 +12,10 @@ text = ' '.join(words)
 masking_image = np.array(Image.open("apple_img.png"))
 
 #4 text와 mask이미지로 단어구름 wordcloud 객체 생성
-wordcloud = WordCloud(width=800, height=400, mask = masking_image, background_color='lightgrey').generate(text)
+wordcloud = WordCloud(mask = masking_image, background_color='lightgrey').generate(text)
 
 #5 wordcloud 객체를 matplotlib을 사용하여 출력
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
+plt.figure(figsize=(8, 8))
+plt.imshow(wordcloud)
 plt.axis('off')
 plt.show()
