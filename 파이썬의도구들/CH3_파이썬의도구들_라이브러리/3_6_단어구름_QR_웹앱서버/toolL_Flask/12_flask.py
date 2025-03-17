f1695162db187a883df7ee26dc93ecd9ec57365d
kukjinm <thater114@gmail.com>
2025-03-17 22:56:27 +0900

    toolL flask commit v2 toolM qrcode commit v1

--- 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_6_단어구름_QR_웹앱서버/toolL_Flask/12_flask.py
+++ 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_6_단어구름_QR_웹앱서버/toolL_Flask/12_flask.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, render_template
 
+#1 Flask 객체 생성
 app = Flask(__name__)
 
+#2 웹 앱 루트 페이지('/')
 @app.route('/', methods=['GET', 'POST'])
-def hello():
+def root_page():
     greeting = None
+    #3 POST 요청 처리
     if request.method == 'POST':
-        name = request.form.get('name', 'World')
-        greeting = f'Hello {name}!'
+        intput_name = request.form.get('intput_name')
+        greeting = f'Hello {intput_name}!'
+    #4 mainpage.html 페이지 렌더링
     return render_template('mainpage.html', greeting=greeting)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#5 웹 앱 실행
+app.run(debug=True)
--- 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_6_단어구름_QR_웹앱서버/toolM_qrcode/13_qrcode.py
+++ 파이썬의도구들/CH3_파이썬의도구들_라이브러리/3_6_단어구름_QR_웹앱서버/toolM_qrcode/13_qrcode.py
@@ -1,22 +1,22 @@
 import qrcode
 
-# Data to be encoded
+#1 QR code에 담을 데이터
 data = "https://github.com/kukjinman/python_tools"
 
-# Create QR code instance
+#2 qrcode 객체 생성
 qr = qrcode.QRCode(
-    version=1,
+    version=3,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
 
-# Add data to the QR code
+#3 QR code에 데이터 추가
 qr.add_data(data)
-qr.make(fit=True)
+# qr.make(fit=True)
 
-# Create an image from the QR code instance
+#4 QR code 이미지 생성
 img = qr.make_image(fill='black', back_color='white')
 
-# Save the image
+#5 QR code 이미지 저장
 img.save("qrcode.png")
