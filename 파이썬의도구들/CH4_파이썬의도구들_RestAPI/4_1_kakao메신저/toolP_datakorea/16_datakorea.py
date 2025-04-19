ac8e1e3d129bacee0a8d5d42737f9dcc576c3125
kukjinm <thater114@gmail.com>
2025-04-19 16:31:13 +0900

    tool16 commit v2

--- 파이썬의도구들/CH4_파이썬의도구들_RestAPI/4_1_kakao메신저/toolP_datakorea/16_datakorea.py
+++ 파이썬의도구들/CH4_파이썬의도구들_RestAPI/4_1_kakao메신저/toolP_datakorea/16_datakorea.py
@@ -1,2 +1,13 @@
-import datakorea
+from datakorea import kakaoapi
 
+REST_API_KEY = "REST API 키"
+REDIRECT_URI = "REDIRECT URI"
+kakao_id = "kakao 아이디"
+kakao_pw = "kakao 비밀번호"
+
+def test_kakaoapi():
+
+    m_kakao = kakaoapi(REST_API_KEY, REDIRECT_URI, kakao_id, kakao_pw)
+    m_kakao.send_text_msg("kakao 메신저 테스트 메세지입니다!")
+
+test_kakaoapi()
