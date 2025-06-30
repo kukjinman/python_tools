fe356a10a767f5f6754c4f73c4dbec8836297611
kukjinm <thater114@gmail.com>
2025-06-30 22:15:43 +0900

    basic code edit commit

--- 파이썬의도구들/CH2_파이썬의기본/4_datatype.py
+++ 파이썬의도구들/CH2_파이썬의기본/4_datatype.py
@@ -1,9 +1,9 @@
 # <데이터를 바로 사용하는 예>
 print(10)
 # <데이터를 변수에 저장하고 사용하는 예>
-a = 20
-print(a)
-print(type(a))
+number = 20
+print(number)
+print(type(number))
 
 
 # 정수
@@ -36,28 +36,3 @@ print(var_int1 + var_int2)
 print(var_str1 + var_str2)
 
 # print(var_int1 + var_str1)
-
-
-#리스트
-var_list = [1,2,3]
-print(var_list)
-print(type(var_list))
-
-var_list2 = [1,2,3,'a',[1,2,'b']]
-print(var_list2)
-print(type(var_list2))
-
-#튜플
-var_tuple = (37.5665, 126.9780)
-print(var_tuple)
-print(type(var_tuple))
-# var_tuple[0] = 1
-
-#딕셔너리
-var_dict={'철수':90,'영희':85,'민수':95}
-print(var_dict)
-print(type(var_dict))
-
-#세트
-var_set = {1, 2, 2, 3, 3, 4, 4}
-print(var_set)
