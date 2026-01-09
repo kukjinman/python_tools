b287e0cfad57aeb26da3624e8a4f158ce9d2e3dd
kukjin.chung <thater114@gmail.com>
2026-01-09 23:10:47 +0900

    kakao fix commit

--- 파이썬의도구들/CH4_파이썬의도구들_RestAPI/4_1_PC_모바일_통신툴/tool15_kakaoAPI/15_kakao_API.py
+++ 파이썬의도구들/CH4_파이썬의도구들_RestAPI/4_1_PC_모바일_통신툴/tool15_kakaoAPI/15_kakao_API.py
@@ -1,33 +1,75 @@
 import requests
 import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 #1 카카오톡 API 사용을 위한 인증 정보 설정
 REST_API_KEY = "REST API 키"
 REDIRECT_URI = "REDIRECT URI"
+kakao_id = "kakao 아이디"
+kakao_pw = "kakao 비밀번호"
 
-#2 토큰을 받기 위한 CODE 값 설정
-CODE = "code 값"
+#7 agreek_to_terms 함수 정의
+def agreek_to_terms(browser):
+    check_box = browser.find_element(by=By.CSS_SELECTOR, value='#line_ctr > label > span.ico_agree.ico_chk')
+    check_box.click()
+    agree_btn = browser.find_element(by=By.CSS_SELECTOR, value='#acceptButton')
+    agree_btn.click()
+    time.sleep(1)
 
-#3 카카오톡 API 인증 URL 생성
-# print(f"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}")
+#3 get_auth_code 함수 정의
+def get_auth_code():
 
-#4 get_new_token 함수 정의
-def get_new_token():
+    opt = webdriver.ChromeOptions()
+    opt.add_experimental_option("detach", True)
+    browser = webdriver.Chrome(options=opt)
 
-    #5 카카오톡 API 토큰 요청 URL
+    #4 browser로 카카오톡 인증 페이지 열기
+    browser.get(f"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}")
+    time.sleep(1)
+
+    #5 카카오톡 로그인 정보 입력 및 로그인 버튼 클릭
+    login_input_id = browser.find_element(by=By.CSS_SELECTOR, value='#loginId--1')
+    login_input_id.send_keys(kakao_id)
+    login_input_pw = browser.find_element(by=By.CSS_SELECTOR, value='#password--2')
+    login_input_pw.send_keys(kakao_pw)
+    login_btn = browser.find_element(by=By.CSS_SELECTOR, value='#mainContent > div > div > form > div.confirm_btn > button.btn_g.highlight.submit')
+    login_btn.click()
+    time.sleep(1)
+
+    #6 현재 URL에서 동의 페이지 확인
+    current_url = browser.current_url
+    # print(current_url)
+
+    if("code=" not in current_url):
+        agreek_to_terms(browser)
+
+    #8최종 인증 코드가 포함된 URL에서 code 값 추출
+    current_url = browser.current_url
+    # print(current_url)
+
+    code = current_url.split("code=")[1]
+    print(f"code: {code}")
+    browser.quit()
+
+    return code
+
+def get_new_token(code_val_):
+
+    #9 카카오톡 API 토큰 발급 요청
     url = 'https://kauth.kakao.com/oauth/token'
-
     data = {
         "grant_type": "authorization_code",
         "client_id": REST_API_KEY,
         "redirect_uri": REDIRECT_URI,
-        "code": CODE
+        "code": code_val_
     }
 
     response = requests.post(url, data=data)
     res_json = response.json()
 
-    #6 access_token 값 추출 및 반환
+    #10 access_token 값 추출 및 반환
     print(res_json)
     try:
         access_token = res_json['access_token']
@@ -37,13 +79,10 @@ def get_new_token():
 
     return access_token
 
-#7 send_msg 함수 정의
 def send_msg(access_token_):
 
-    #8 카카오톡 API 메시지 전송 URL
+    #11 카카오톡 메시지 전송
     url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
-
-    #9 메시지 전송을 위한 헤더와 template_object 설정
     headers = {
         "Authorization": f"Bearer {access_token_}"
     }
@@ -59,6 +98,7 @@ def send_msg(access_token_):
     print(response.status_code)
     print(response.json())
 
-#10 get_new_token 함수 호출 및 send_msg 함수 호출
-token = get_new_token()
+#2 코드 실행
+code_val = get_auth_code()
+token = get_new_token(code_val)
 send_msg(token)
